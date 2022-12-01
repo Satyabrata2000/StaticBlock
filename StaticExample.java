4b71edefc0e89d2bbdf0ee3c57e79240426bec85
Satyabrata2000 <satyabrata2000@gmail.com>
2022-12-01 07:12:52 -0600

    Static Block

---
+++ StaticExample.java
@@ -0,0 +1,14 @@
+package StaticBlock;
+
+public class StaticExample {
+
+    static {
+
+        System.out.println("String 1");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("String 2");
+    }
+}
